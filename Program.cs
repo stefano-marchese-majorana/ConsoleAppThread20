54f954f32f18e5df7a6b356a7f67f062be478bee
stefano-marchese-majorana <stefano.marchese.majorana@gmail.com>
2022-03-25 05:21:52 +0100

    full code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿// esempio con tutto quello visto negli esempi precendenti
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ConsoleAppThread20
 {
     public class MyData
     {
+        public static int MaxLoopNumber = 5;
+        public static bool IsBackground = false;
+
         public static object DataLock = new object();
         public static int DataShared { get; set; }
     }
@@ -15,22 +19,41 @@ namespace ConsoleAppThread20
     {
 
         // Method1
-        public static void Method()
+        public static void Method(Object slp)
         {
-            for (int i = 0; i < 10; i++)
+            int sleep = Convert.ToInt32(slp);
+
+            string name = Thread.CurrentThread.Name;
+            ThreadPriority priority = Thread.CurrentThread.Priority;
+            Type type = Thread.CurrentThread.GetType();
+            int tid = Thread.CurrentThread.ManagedThreadId;
+
+            Console.Write($"Thread({name}) ");
+            Console.Write($"sleep({slp}) ");
+            Console.Write($"tid({tid}) ");
+            Console.Write($"prioprity({priority}) ");
+            Console.WriteLine($"type({type})");
+
+            for (int i = 0; i < MyData.MaxLoopNumber; i++)
             {
                 lock (MyData.DataLock)
                 {
                     int temp = MyData.DataShared;
-                    Console.WriteLine($"THREAD {Thread.CurrentThread.Name}" +
-                        $"({i})({MyData.DataShared})");
+                    Console.WriteLine($"Thread({name}) " +
+                        $"Loop({i}) " +
+                        $"DataShared({MyData.DataShared})");
                     Thread.Yield();
                     MyData.DataShared = temp + 1;
-                    Console.WriteLine($"THREAD {Thread.CurrentThread.Name}" +
-                        $"({i})({MyData.DataShared})");
+                    Console.WriteLine($"Thread({name}) " +
+                        $"Loop({i}) " +
+                        $"DataShared({MyData.DataShared})");
                 }
+
+                Console.WriteLine($"Sleeping Thread({name}) Sleep({sleep})");
+                Thread.Sleep(sleep);
             }
-            Console.WriteLine($"THREAD {Thread.CurrentThread.Name} stopping");
+
+            Console.WriteLine($"Stopping Thread({name})");
         }
     }
 
@@ -39,26 +62,44 @@ namespace ConsoleAppThread20
         static void Main(string[] args)
         {
             string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-            Thread[] ths = new Thread[4];
+            List<Thread> ths = new List<Thread>();
 
-            int i = 0;
             foreach (string s in cars)
             {
-                Console.WriteLine($"Creating and starting thread {s}");
-                ths[i] = new Thread(TH.Method);
-                ths[i].Name = s;
-                ths[i].IsBackground = false;
-                ths[i].Start();
-                i++;
+                Console.WriteLine($"Creating thread {s}");
+                Thread th = new Thread(TH.Method);
+                th.Name = s;
+                th.IsBackground = MyData.IsBackground;
+                ths.Add(th);
             }
 
             foreach (Thread th in ths)
             {
-                th.Join();
+                Random rd = new Random();
+                int sleep = rd.Next(0, 6) * 1000;
+                th.Start(sleep);
+            }
+
+            foreach (Thread th in ths)
+            {
+                Console.WriteLine($"Main tests {th.Name} is alive: {th.IsAlive}");
+            }
+
+            if (!MyData.IsBackground)
+            {
+                foreach (Thread th in ths)
+                {
+                    th.Join();
+                }
+            }
+
+            foreach (Thread th in ths)
+            {
+                Console.WriteLine($"Main tests {th.Name} is alive: {th.IsAlive}");
             }
 
             Console.WriteLine($"Data shared {MyData.DataShared}");
-            Console.WriteLine($"Main thread stopping");
+            Console.WriteLine($"Stopping Main thread");
         }
     }
 }
